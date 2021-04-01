575044d1777590c1ee5e4c8aa015e20f5964d857
emmanuel lopez <emmanuel.lopez.g@icloud.com>
2021-03-31 22:24:30 -0600

    first commit 🚀

---
+++ csharp_notes/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace csharp_notes
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            
+        }
+    }
+}
