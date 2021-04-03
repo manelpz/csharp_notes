7953f6fc0ebfe1ea045466de6402b043e3c7f678
emmanuel lopez <emmanuel.lopez.g@icloud.com>
2021-04-02 21:25:04 -0600

    .

---
+++ csharp_notes/Customer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace csharp_notes
+{
+    public class Customer
+    {
+        
+        public int Id;
+        public string Name;
+
+        public Customer(int id)
+        {
+            this.Id = id;
+        }
+   
+    }       
+}
