5fcc10ee78f9e6af6ea65afb905d9749b996c99b
emmanuel lopez <emmanuel.lopez.g@icloud.com>
2021-04-12 23:50:29 -0500

    .

---
+++ csharp_notes/OperationResult.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace csharp_notes
+{
+    public class OperationResult
+    {
+        public boll success => !MessageList.Any();
+        public Persona Persona { get; set; }
+
+        public OperationResult()
+        {
+            MessageList = new GenericList<string>();
+        }
+
+
+        public void AddMessage(string message)
+        {
+            MessageList.Add(message);
+        }
+
+
+
+        public void SetSuccesResponse(Persona pers) {
+            Persona = pers;
+        }
+    }
+}
