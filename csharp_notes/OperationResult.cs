9501cf03a385b73e5591ac652af160f5ef99bef7
emmanuel lopez <emmanuel.lopez.g@icloud.com>
2021-04-21 09:16:36 -0500

    .

--- csharp_notes/OperationResult.cs
+++ csharp_notes/OperationResult.cs
@@ -24,6 +24,22 @@ namespace csharp_notes
         }
 
 
+        var book = new Book { isbn = "111", title="c#"};
+
+            var numbers = new List();
+
+            numbers.Add(10);
+
+
+            var books = new BookList();
+            books.Add(book);
+
+            var numbers = new GenericList<int>();
+            numbers.Add(10);
+
+            var books = new GenericList<Book>();
+            books.Add(NewsStyleUriParser Books());
+
 
     }
 }
